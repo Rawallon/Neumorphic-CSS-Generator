6bfabde85a31831e22373a04702ab9e36ac871ac
OTalDoDante <rawallon@gmail.com>
2020-09-04 19:49:54 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,169 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Configurator } from "./component/Configurator";
+import { Preview } from "./component/Preview";
+
+function createShades(col: string, amt: number) {
+  let hex: string = String(col).replace(/[^0-9a-f]/gi, "");
+  if (hex.length < 6) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+  var rgb = "#",
+    c,
+    i;
+  for (i = 0; i < 3; i++) {
+    c = parseInt(hex.substr(i * 2, 2), 16);
+    c = Math.round(Math.min(Math.max(0, c + c * amt), 255)).toString(16);
+    rgb += ("00" + c).substr(c.length);
+  }
+  return rgb;
+}
 
 function App() {
+  const [size, setSize] = useState(300);
+  const [radius, setRadius] = useState(50);
+  const [dist, setDist] = useState(20);
+  const [intensity, setIntensity] = useState(0.15);
+  const [blur, setBlur] = useState(60);
+  const [background, setBackground] = useState("#efeeee");
+  const [bgInput, setBgInput] = useState(background);
+  const [shape, setShape] = useState(0);
+  const [angle, setAngle] = useState(0);
+  useEffect(() => {
+    document.documentElement.style.setProperty("--background", background);
+    if (shape === 0 || shape === 402) {
+      document.documentElement.style.setProperty(
+        "--backgroundFirst",
+        background
+      );
+      document.documentElement.style.setProperty(
+        "--backgroundSecond",
+        background
+      );
+    } else if (shape === 1) {
+      document.documentElement.style.setProperty(
+        "--backgroundFirst",
+        createShades(background, 0.1 * -1)
+      );
+      document.documentElement.style.setProperty(
+        "--backgroundSecond",
+        createShades(background, 0.1)
+      );
+    } else if (shape === 2) {
+      document.documentElement.style.setProperty(
+        "--backgroundFirst",
+        createShades(background, 0.1)
+      );
+      document.documentElement.style.setProperty(
+        "--backgroundSecond",
+        createShades(background, 0.1 * -1)
+      );
+    }
+
+    document.documentElement.style.setProperty("--blur", String(blur) + "px");
+    document.documentElement.style.setProperty(
+      "--positionX",
+      String(dist) + "px"
+    );
+    document.documentElement.style.setProperty(
+      "--positionY",
+      String(dist) + "px"
+    );
+    document.documentElement.style.setProperty(
+      "--positionXOpposite",
+      "-" + String(dist) + "px"
+    );
+    document.documentElement.style.setProperty(
+      "--positionYOpposite",
+      "-" + String(dist) + "px"
+    );
+    document.documentElement.style.setProperty(
+      "--intensityL",
+      createShades(background, intensity)
+    );
+    document.documentElement.style.setProperty(
+      "--intensityD",
+      createShades(background, intensity * -1)
+    );
+    
+  }, [background, dist, intensity, blur, shape]);
+
+
+  useEffect(() => {
+
+    switch (angle) {
+      case 0:
+        document.documentElement.style.setProperty("--positionX",String(dist) + "px");
+        document.documentElement.style.setProperty("--positionY",String(dist) + "px");
+        document.documentElement.style.setProperty("--positionXOpposite","-" + String(dist) + "px");
+        document.documentElement.style.setProperty("--positionYOpposite","-" + String(dist) + "px");
+        document.documentElement.style.setProperty("--angle","145deg");
+        break;
+      case 1:
+        document.documentElement.style.setProperty("--positionX","-"+String(dist) + "px");
+        document.documentElement.style.setProperty("--positionY",String(dist) + "px");
+        document.documentElement.style.setProperty("--positionXOpposite", String(dist) + "px");
+        document.documentElement.style.setProperty("--positionYOpposite","-" + String(dist) + "px");
+        document.documentElement.style.setProperty("--angle","225deg");
+        break;
+      case 2:
+        document.documentElement.style.setProperty("--positionX","-" +String(dist) + "px");
+        document.documentElement.style.setProperty("--positionY","-" +String(dist) + "px");
+        document.documentElement.style.setProperty("--positionXOpposite",String(dist) + "px");
+        document.documentElement.style.setProperty("--positionYOpposite",String(dist) + "px");
+        document.documentElement.style.setProperty("--angle","315deg");
+        break;
+      case 3:
+        document.documentElement.style.setProperty("--positionX",String(dist) + "px");
+        document.documentElement.style.setProperty("--positionY","-" + String(dist) + "px");
+        document.documentElement.style.setProperty("--positionXOpposite","-" + String(dist) + "px");
+        document.documentElement.style.setProperty("--positionYOpposite",String(dist) + "px");
+        document.documentElement.style.setProperty("--angle","45deg");
+        break;
+    
+      default:
+        document.documentElement.style.setProperty("--angle",
+          "145deg"
+        );
+        break;
+    }
+  }, [angle])
+
+  const setBG = (color: string) => {
+    setBgInput(color);
+    color.length === 7 && color.charAt(0) === "#"
+      ? setBackground(color)
+      : setBgInput(color);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Configurator
+        size={size}
+        setSize={setSize}
+        bg={background}
+        setBg={setBackground}
+        bgGF={shape > 0 ? createShades(background, 0.1 * -1) : background}
+        bgGS={shape > 0 ? createShades(background, 0.1) : background}
+        bgInput={bgInput}
+        setBgInput={setBG}
+        radius={radius}
+        setRadius={setRadius}
+        dist={dist}
+        setDist={setDist}
+        intensity={intensity}
+        setIntensity={setIntensity}
+        blur={blur}
+        setBlur={setBlur}
+        colors={
+          createShades(background, intensity) +
+          "-" +
+          createShades(background, intensity * -1)
+        }
+        shape={shape}
+        setShape={setShape}
+      />
+      <Preview size={size} radius={radius} shape={shape} angle={angle} setAngle={setAngle} />
     </div>
   );
 }
---
+++ src/component/Preview.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import "./Preview.css";
+
+interface props {
+  size: number;
+  radius: number;
+  shape: number;
+  angle: number;
+  setAngle: (n: number) => void;
+}
+
+export const Preview: React.FC<props> = ({
+  size,
+  radius,
+  shape,
+  angle,
+  setAngle,
+}) => {
+  useEffect(() => {
+    document.documentElement.style.setProperty("--size", String(size) + "px");
+    document.documentElement.style.setProperty(
+      "--radius",
+      String(radius) + "px"
+    );
+  }, [size, radius]);
+  return (
+    <div className="preview">
+      <button className={`leftU${angle === 0 ? " active" : ""}`} onClick={() => setAngle(0)}></button>
+      <button className={`rightU${angle === 1 ? " active" : ""}`} onClick={() => setAngle(1)}></button>
+      <button className={`rightD${angle === 2 ? " active" : ""}`} onClick={() => setAngle(2)}></button>
+      <button className={`leftD${angle === 3 ? " active" : ""}`} onClick={() => setAngle(3)}></button>
+      <div className={`box${shape === 3 ? " pressed" : ""}`}></div>
+    </div>
+  );
+};
