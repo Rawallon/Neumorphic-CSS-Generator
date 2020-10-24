ef3d9996dd03029835466fa4b97360759fbab2ff
OTalDoDante <rawallon@gmail.com>
2020-10-24 14:26:14 -0300

    Updates

---
+++ precache-manifest.3076ceb68a594c36c1c9e0f0284a8283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e76c10d0131465f775e8a9c228f560",
+    "url": "/Neumorphic-CSS-Generator/index.html"
+  },
+  {
+    "revision": "aa32b99b75ccdce826f2",
+    "url": "/Neumorphic-CSS-Generator/static/css/main.f2b1e9a8.chunk.css"
+  },
+  {
+    "revision": "18aed12c36c0b311dbac",
+    "url": "/Neumorphic-CSS-Generator/static/js/2.5e73733c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Neumorphic-CSS-Generator/static/js/2.5e73733c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa32b99b75ccdce826f2",
+    "url": "/Neumorphic-CSS-Generator/static/js/main.a30e2541.chunk.js"
+  },
+  {
+    "revision": "3de21c9dbbf6c6c6a9ea",
+    "url": "/Neumorphic-CSS-Generator/static/js/runtime-main.1653891f.js"
+  }
+]);
